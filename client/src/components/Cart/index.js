c857126da3dff2c76ffce3558daabf35c8b3fa4a
Bobby T <bob.tarino@gmail.com>
2021-08-22 16:03:32 -0500

    redirect to stripe on frontend

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -5,11 +5,19 @@ import CartItem from '../CartItem';
 import Auth from '../../utils/auth';
 import './style.css';
 import { useStoreContext } from '../../utils/GlobalState';
+import { QUERY_CHECKOUT } from '../../utils/queries';
+import { loadStripe } from '@stripe/stripe-js';
+import { useLazyQuery } from '@apollo/client';
+
+
+// use this object to perform checkout redirect
+const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
 
 const Cart = () => {
     
     const [state, dispatch] = useStoreContext();
+    const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
 
     // if state.cart.length is 0, then executing getCart() to retrieve the items from the cart object store and save it to the global state object
     useEffect(() => {
@@ -22,6 +30,14 @@ const Cart = () => {
           getCart();
         }
       }, [state.cart.length, dispatch]);
+    
+    useEffect(() => {
+        if (data) {
+          stripePromise.then((res) => {
+            res.redirectToCheckout({ sessionId: data.checkout.session });
+          });
+        }
+    }, [data]);
 
     function toggleCart() {
         dispatch({ type: TOGGLE_CART });
@@ -33,6 +49,20 @@ const Cart = () => {
           sum += item.price * item.purchaseQuantity;
         });
         return sum.toFixed(2);
+    }
+
+    function submitCheckout() {
+        const productIds = [];
+      
+        state.cart.forEach((item) => {
+          for (let i = 0; i < item.purchaseQuantity; i++) {
+            productIds.push(item._id);
+          }
+        });
+
+        getCheckout({
+            variables: { products: productIds }
+          });
     }
 
     if (!state.cartOpen) {
@@ -60,7 +90,7 @@ const Cart = () => {
                         <strong>Total: ${calculateTotal()}</strong>
                         {
                             Auth.loggedIn() ?
-                                <button>
+                                <button onClick={submitCheckout}>
                                     Checkout
                                 </button>
                                 :
